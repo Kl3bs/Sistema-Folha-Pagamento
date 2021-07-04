c6b6e86efa496a12de660f1c5893e5ba5a5ecd6e
Kleber Vasconcelos <kleberjunior58@gmail.com>
2021-07-04 11:00:47 -0300

    Form update

--- app/models.py
+++ app/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.db.models.enums import Choices
+from django import forms
+
 
 # Create your models here.
+
+TITLE_CHOICES = [
+    ('MR', 'Mr.'),
+    ('MRS', 'Mrs.'),
+    ('MS', 'Ms.'),
+]
 class Funcionario(models.Model) :
     nome = models.CharField(max_length=35)
     endereco = models.CharField(max_length=40)
-    tipo = models.CharField(max_length=15)
+    #tipo = forms.ChoiceField(widget=forms.Select(attrs={'class': 'selector'}))
+    tipo_prof = models.TextChoices('MedalType', 'HORISTA MENSALISTA COMISSIONADO')
+    tipo = models.CharField(blank=True, choices=tipo_prof.choices, max_length=12)
