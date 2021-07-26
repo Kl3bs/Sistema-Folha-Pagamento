20836c61cf0a62956efd3a8c0c9736fbebf9c807
Kleber Vasconcelos <kleberjunior58@gmail.com>
2021-07-26 19:42:25 -0300

    Update home

--- app/static/custom.js
+++ app/static/custom.js
@@ -1,31 +1,33 @@
 $(document).ready(function () {
   $("#id_data_ponto").datepicker({ changeYear: true, changeMonth: true });
-});
 
-$("#id_data_venda").datepicker({ changeYear: true, changeMonth: true });
+  $("#id_data_venda").datepicker({ changeYear: true, changeMonth: true });
 
-//TIME PICKER CONFIG
+  //TIME PICKER CONFIG
 
-$("#id_hora_entrada").timepicker({
-  timeFormat: "HH:mm",
-  interval: 60,
-  minTime: "8",
-  maxTime: "6:00pm",
-  defaultTime: "8",
-  startTime: "08:00",
-  dynamic: false,
-  dropdown: true,
-  scrollbar: true,
-});
+  // $("#id_valor_venda").mask("0000.000");
 
-$("#id_hora_saida").timepicker({
-  timeFormat: "HH:mm",
-  interval: 60,
-  minTime: "8",
-  maxTime: "6:00pm",
-  defaultTime: "18",
-  startTime: "08:00",
-  dynamic: false,
-  dropdown: true,
-  scrollbar: true,
+  $("#id_hora_entrada").timepicker({
+    timeFormat: "HH:mm",
+    interval: 60,
+    minTime: "8",
+    maxTime: "6:00pm",
+    defaultTime: "8",
+    startTime: "08:00",
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true,
+  });
+
+  $("#id_hora_saida").timepicker({
+    timeFormat: "HH:mm",
+    interval: 60,
+    minTime: "8",
+    maxTime: "6:00pm",
+    defaultTime: "18",
+    startTime: "08:00",
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true,
+  });
 });
